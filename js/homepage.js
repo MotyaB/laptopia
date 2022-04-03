d3081d0cc11d3bfe9c46ecbcc1fd9b16a88116f7
motyasas <61905156+MotyaB@users.noreply.github.com>
2022-04-03 12:43:22 +0700

    Add files via upload

--- js/homepage.js
+++ js/homepage.js
@@ -37,120 +37,13 @@ timer = setInterval(function() {
 	}
 },0)
 
-let elements = document.querySelectorAll('.element')
+var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()))
+if (mobile) { 
+	alert("Мобильное устройство!!")
+	$('.navWrap').hide()
+} 
 
-// for (element of elements) {
-// 	element.addEventListener('focus', function(){
-// 		this.style = "background-color: lightblue; border-color: red; border-width: 3px;"
-// 	})
-
-// 	element.addEventListener('blur', function(){
-// 		this.style = "background-color: white; border-color: white; border-width: 1px;"
-// 	})
-// }
-
-function validate() {
-	let elements = document.querySelectorAll('.element')
-
-	for (element of elements) {
-		if (element.value == '') {
-			alert(`Поле ${element.name} должно быть заполнено!`)
-			return false
-		}
-	}
-	return true
+else 
+{ 
+	alert("НЕ мобильное устройство!")
 }
-
-// // // // хранится на пк человека
-
-// // // localStorage.setItem("user", "admin")
-// // // localStorage.setItem("role", "administrator")
-// // // // alert("Hi, "+localStorage.getItem("user"))
-// // // localStorage.removeItem("user")
-// // // console.log(localStorage.getItem("role"))
-
-// // // // хранится во время сессии
-
-// // sessionStorage.setItem("user", "admin")
-// // sessionStorage.setItem("role", "administrator")
-// // alert("Hi, "+sessionStorage.getItem("user"))
-// // // sessionStorage.removeItem("user")
-// // sessionStorage.setItem("user", "name")
-// // console.log(sessionStorage.getItem("role"))
-
-
-// // setTimeout(function(){
-// // 	let a = document.querySelector("#func").value
-// // 	let func = Function(a)
-// // func()
-// // }, 6000)
-
-// // alert("Hi"); a=1; b=2; alert(a+b)
-
-// // let cart = {
-// // 	"name": "adm",
-// // 	"role": "admin",
-// // 	"mail": "text@gmail.com",
-
-// // 	toString() {
-// // 		return `name: ${this.name}\nrole: ${this.role}\nmail: ${this.mail}`
-// // 	}
-// // }
-
-// // let b = `"name": "adm", "role": "admin", "mail:" "text@gmail.com"`
-
-// // cart2 = JSON.parse(b)
-// // alert(cart2.name)
-
-// // let f = JSON.stringify(cart)
-// // localStorage.setItem("cart", f)
-
-// // let cart1 = JSON.parse(localStorage.getItem("cart"))
-// // alert(cart1.name)
-// // alert(cart1)
-// // alert(cart)
-
-// // alert(f)
-
-// // alert(cart)
-// // console.log(cart["name"])
-// // console.log(cart.name)
-
-// localStorage.removeItem('cart')
-
-// document.querySelector('#cart').addEventListener('click', function(){
-// 	alert(localStorage.getItem('cart'))
-// })
-
-// let items = document.querySelectorAll('.items')
-
-// for (let item of items) {
-// 	item.addEventListener('click', addToCart)
-// }
-
-// function getFromCart() {
-// 	return JSON.parse(localStorage.getItem('cart'))
-// }
-
-// function addToCart() {
-// 	let itemID = this.dataset.id
-// 	let itemName = this.dataset.name
-// 	let itemPrice = this.dataset.price
-
-// 	let cart = getFromCart() || {}
-
-// 	if (cart.hasOwnProperty(itemID)) {
-// 		cart[itemID]['cnt'] ++
-// 	}
-// 	else {
-
-// 		let item = {'name': itemName, 'price': itemPrice, 'cnt': 1}
-// 		cart[itemID] = item
-
-// 	}
-
-
-// 	localStorage.setItem('cart', JSON.stringify(cart))
-// }
-
-
