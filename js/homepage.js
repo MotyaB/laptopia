2429efe582b8045ab866e806a9b30a623cf14286
motyasas <61905156+MotyaB@users.noreply.github.com>
2022-04-23 19:19:50 +0700

    Add files via upload

--- js/homepage.js
+++ js/homepage.js
@@ -3,7 +3,6 @@ if (mobile) {
 	window.location.href = 'getTheHellOut.html';
 } 
 
-
 let timer
 
 function addZero(number) {
@@ -42,3 +41,76 @@ timer = setInterval(function() {
 		alert("Sale is over!")
 	}
 },0)
+
+var d = document,
+    itemBox = d.querySelectorAll('.item_box')
+    cartCont = d.getElementById('cart_content')
+
+function addEvent(elem, type, handler){
+  if(elem.addEventListener){
+    elem.addEventListener(type, handler, false);
+  } else {
+    elem.attachEvent('on'+type, function(){ handler.call( elem ); });
+  }
+  return false;
+}
+
+function getCartData(){
+  return JSON.parse(localStorage.getItem('cart'));
+}
+
+function setCartData(o){
+  localStorage.setItem('cart', JSON.stringify(o));
+  return false;
+}
+
+function addToCart(e){
+  this.disabled = true
+  var cartData = getCartData() || {}, 
+      parentBox = this.parentNode, 
+      itemId = this.getAttribute('data-id'), 
+      itemTitle = parentBox.querySelector('.item_title').innerHTML, 
+      itemPrice = parentBox.querySelector('.item_price').innerHTML; 
+  if(cartData.hasOwnProperty(itemId)){ 
+    cartData[itemId][2] += 1;
+  } else { 
+    cartData[itemId] = [itemTitle, itemPrice, 1];
+  }
+  if(!setCartData(cartData)){ 
+    this.disabled = false; 
+  }
+ return false;
+}
+
+for(var i = 0; i < itemBox.length; i++){
+  addEvent(itemBox[i].querySelector('.add_item'), 'click', addToCart);
+}
+
+function openCart(e){
+  var cartData = getCartData(), 
+      totalItems = '';
+
+  if(cartData !== null){
+    totalItems = '<table class="shopping_list"><tr><th>Product name</th><th>Price</th><th>Quantity</th></tr>';
+    for(var items in cartData){
+      totalItems += '<tr>';
+      for(var i = 0; i < cartData[items].length; i++){
+        totalItems += '<td>' + cartData[items][i] + '</td>';
+      }
+      totalItems += '</tr>';
+    }
+    totalItems += '<table>';
+    cartCont.innerHTML = totalItems;
+  } else {
+
+    cartCont.innerHTML = 'The cart is empty!';
+  }
+  return false;
+}
+
+addEvent(d.getElementById('checkout'), 'click', openCart);
+
+addEvent(d.getElementById('clear_cart'), 'click', function(e){
+  localStorage.removeItem('cart');
+  cartCont.innerHTML = 'The cart is cleared.';
+});
